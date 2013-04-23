8e00541c4518b930a6729fc1a612924960b01f23
Dan Lazewatsky <dan@lazewatsky.com>
2013-04-23 16:23:41 -0500

    made errors less mysterious

--- citation_manager/dblp_scraper/spiders/dblp_spider.py
+++ citation_manager/dblp_scraper/spiders/dblp_spider.py
@@ -1,15 +1,26 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from dblp_scraper.items import DblpScraperItem, find_authors
+import django.db.utils
+from models import PubAuthor, Author
 
-from models import PubAuthor
+from django.template.defaultfilters import slugify as _slugify
+def slugify(text):
+	return _slugify(text).replace('-','_')
 
 class DBLPSpider(BaseSpider):
 	name = 'dblp'
 	allowed_domains = ['dblp.org']
-	start_urls = [
-		'http://www.dblp.org/search/api/?q=ce:author:william_d_smart*&h=1000&c=4&f=0&format=xml' # fill this in programatically
-	]
+	url_format = 'http://www.dblp.org/search/api/?q=ce:author:%s*&h=1000&c=4&f=0&format=xml'
+	start_urls = []
+	# 	'http://www.dblp.org/search/api/?q=ce:author:william_d_smart*&h=1000&c=4&f=0&format=xml' # fill this in programatically
+	# ]
+
+	def __init__(self, *args, **kwargs):
+		super(DBLPSpider, self).__init__(*args, **kwargs)
+		# import pdb; pdb.set_trace()
+		for author in Author.objects.filter(user_id__isnull=False):
+			self.start_urls.append(self.url_format % slugify(author.published_name))
 
 	def parse(self, request):
 		hxs = HtmlXPathSelector(request)
@@ -17,13 +28,16 @@ class DBLPSpider(BaseSpider):
 			item = DblpScraperItem()
 			for attr, (selector, func) in item.mappings.iteritems():
 				item[attr] = func(hit.select(selector).extract())
-			print 'Adding ', item['title']
-
 			# need to do this by hand so we have access to the instance for adding authors
+			# import pdb; pdb.set_trace()
 			modelargs = dict((k, item.get(k)) for k in item._values if k in item._model_fields)
 			instance = item.django_model(**modelargs)
-
-			instance.save()
+			try:
+				instance.save()
+				print 'Adding ', item['title']
+			except django.db.utils.IntegrityError, e:
+				# ignore things we've already imported
+				continue
 			for n, author in enumerate(find_authors(hit.select('descendant::author/text()').extract())):
 				author.save()
 				pa = PubAuthor(author=author, publication=instance, author_num=n)
